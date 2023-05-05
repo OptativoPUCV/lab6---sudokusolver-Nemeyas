c8d46816b44baf0a34204c5228b9b223a1a52faf
Replit user <>
2023-05-05 00:17:35 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -78,7 +78,6 @@ int is_valid(Node* n){
     for(p=0; p<9; p++){ 
       int i=3*(k/3) + (p/3) ;
       int j=3*(k%3) + (p%3) ;
-      matrizaux = n->sudo[i][j];
       if(n->sudo[i][j] != 0){ 
         if(matrizaux[n->sudo[i][j]] == 1){
           return 0;
